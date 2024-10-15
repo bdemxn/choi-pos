cc8a5fc679e554041d862e67d0aaece8dcca7e33
Kevin Bonilla <kb1909zzz@gmail.com>
2024-10-15 09:00:26 -0600

    implementation(cards): cards creation

--- src/widgets/auth/index.js
+++ src/widgets/auth/index.js
@@ -1,7 +1,11 @@
 import Button from "./Button.astro";
 import AuthForm from "./AuthForm.astro";
+import Card from "./Cards.astro"
+import Navbar from "./Navbar.astro";
 
 export {
   Button,
-  AuthForm
+  AuthForm,
+  Card,
+  Navbar
 }
